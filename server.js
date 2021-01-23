77afa50a2abbbda32bb1e754531e89368e3833b8
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-01-23 12:43:06 -0600

    can use postman on server

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 const characters = [
     {
-    name: 'Yoda',
-    role: 'Jedi Master',
+    name: "Yoda",
+    role: "Jedi Master",
     forcePoints: 100000,
     age: 900,
     avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/baby-yoda-old-yoda-1574103229.jpg?crop=0.486xw:0.973xh;0.514xw,0&resize=480:*',
@@ -43,8 +46,12 @@ app.get('/api/characters/:routeName', (req, res) => {
         return character.routeName === targetCharacter
     })
     res.json(character)
-})
+    })
 
+app.post('/api/characters/add', (req, res) => {
+    console.log(req.body)
+    res.end()
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
